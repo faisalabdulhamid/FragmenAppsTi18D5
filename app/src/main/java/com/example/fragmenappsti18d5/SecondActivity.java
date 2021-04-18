8cc62b6164a532c9d67954056c80cd68623565d9
faisalabdulhamid <faisalhamid235@gmail.com>
2021-04-18 14:59:22 +0700

    FragmenAppsTi18D5

---
+++ app/src/main/java/com/example/fragmenappsti18d5/SecondActivity.java
@@ -0,0 +1,27 @@
+package com.example.fragmenappsti18d5;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void  onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        Log.v("CREATING","SECOND ACTIVITY");
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            finish();
+            return;
+        }
+        if (savedInstanceState == null){
+            SecondFragment details = new SecondFragment();
+            getFragmentManager().beginTransaction().add(android.R.id.content, details).commit();
+        }
+    }
+}
